3f72197518306070066a052cc06e3e739fb9417f
Rafael Pelle <rafapelle@gmail.com>
2019-01-27 18:03:31 +0000

    section 2, challenge 4 (solved #01)

--- 2-dom/desafio-04/app.js
+++ 2-dom/desafio-04/app.js
@@ -1,11 +1,25 @@
 new Vue({
 	el: '#desafio',
 	data: {
-
+		efeitoActive: false,
+		intervalId: '',
+	},
+	computed: {
+		efeitoStyle() {
+			return {
+				destaque: this.efeitoActive,
+				encolher: !this.efeitoActive,
+			}
+		},
 	},
 	methods: {
 		iniciarEfeito() {
-
+			this.intervalId = setInterval(() => {
+				this.efeitoActive = !this.efeitoActive
+			}, 100)
+		},
+		finalizarEfeito() {
+			clearInterval(this.intervalId)
 		},
 		iniciarProgresso() {
 
