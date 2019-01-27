82c2c2924fdbc98cfd60712ea01969662cbe6467
Rafael Pelle <rafapelle@gmail.com>
2019-01-27 16:19:15 +0000

    section 2, challenge 3 (solved)

--- 2-dom/desafio-03/app.js
+++ 2-dom/desafio-03/app.js
@@ -1,6 +1,18 @@
 new Vue({
     el: '#desafio',
     data: {
-        valor: 0
-    }
+        valor: 0,
+    },
+    computed: {
+        resultado() {
+            return this.valor === 37 ? 'Valor igual' : 'Valor diferente'
+        }
+    },
+    watch: {
+        resultado(newValue, oldValue) {
+            setTimeout(() => {
+                this.valor = 0
+            }, 5000)
+        },
+    },
 });
