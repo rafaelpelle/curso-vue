29dd5ad4678db8f040e9557b5f37ff9cf38846bb
Rafael Pelle <rafapelle@gmail.com>
2019-02-03 18:46:17 +0000

    section 3, lesson 53 (challenge 5 solved)

--- 3-condicionais-e-listas/desafio-05/app.js
+++ 3-condicionais-e-listas/desafio-05/app.js
@@ -1,6 +1,7 @@
 new Vue({
 	el: '#desafio',
 	data: {
+		visible: false,
 		array: ['Pedro', 'Bia', 'Ana', 'Rebeca'],
 		livro: {
 			titulo: 'O Senhor dos Anéis',
@@ -11,6 +12,7 @@ new Vue({
 			id: 10,
 			nome: 'Maria',
 			notas: [7.67, 8.33, 6.98, 9.21]
-		}
+		},
+		colors: ['Red', 'Blue', 'Green', 'White', 'Black'],
 	}
 });
